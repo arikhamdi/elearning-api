61ffcce4d6822b6249138e287710d7be6a57ba39
Arik <arik.hamdi@gmail.com>
2021-05-29 03:36:47 +0200

    postgreSQL configured

--- e-learning-front/src/config.js
+++ e-learning-front/src/config.js
@@ -1,5 +1,5 @@
 // export const API = process.env.REACT_APP_API_URL
 // export const API = process.env.REACT_APP_API_MEDIA_ROOT
-export const API = "http://172.0.0.1/api"
+export const API = "http://127.0.0.1:8000/api"
 
-export const MEDIA_ROOT = "http://172.0.0.1:8000"
+export const MEDIA_ROOT = "http://127.0.0.1:8000"
